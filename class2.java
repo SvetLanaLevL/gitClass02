827bb06a0f6a70c67d60db5954d9ef40438b0ab4
SvetLanaLevL <lanabonitas@gmail.com>
2025-05-13 21:22:51 -0400

    thisrdcommit

--- class2.java
+++ class2.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class class2 {
+public class class2 {
+    public static void main(String[] args) {
+
+        int counter = 0;
+        while(counter<=8) {
+            System.out.println(counter);
+            counter += 1;
+        }
+    }
 }
