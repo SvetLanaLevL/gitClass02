7e2f5ceb99ad17a919cce7d82e00bc970869aed6
SvetLanaLevL <lanabonitas@gmail.com>
2025-05-13 21:50:36 -0400

    thirdcommit

--- class2.java
+++ class2.java
@@ -1,10 +1,8 @@
 public class class2 {
     public static void main(String[] args) {
 
-        int counter = 0;
-        while(counter<=8) {
-            System.out.println(counter);
-            counter += 1;
+        System.out.println("Hello World!");
+        System.out.println("second commit");
         }
     }
 }
